65d8591cd006ebd8cec28d11358bda2c8ac48421
yomZsamora <samora.y@adzumi.co.ke>
2022-01-20 19:26:12 +0300

    commit tree c39d76bc8d4d3f31d044eac8c240c2704dfb34e8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let superbowlWin = superbowlWinRecord => {
+	// let superbowlWon = superbowlWinRecord.find(superbowl => superbowl.result === "W");
+	// return superbowlWon.year;
+	
+	let superbowlWon = superbowlWinRecord.find( superbowl => {  return superbowl.result === "W" })
+	if (superbowlWon){ 
+		return superbowlWon.year
+	} else {
+		return undefined
+	}
+}
+
 
